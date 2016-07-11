ec5844fb8433118661cef5369d6b95fc3238f975
Ten Wong <wangtengoo7@gmail.com>
2016-07-11 15:42:07 +0800

    add seeed account login

--- wio/commands/cmd_login.py
+++ wio/commands/cmd_login.py
@@ -1,11 +1,15 @@
 import click
+import sys
+import requests
+import time
+import hashlib
 from wio import termui
+from wio import config
 from wio.wio import pass_wio
 from wio.wio import login_endpoint
 from wio.wio import verify
-from wio.wio import login_server
+from wio.wio import choise_server
 
-import requests
 
 @click.command()
 @pass_wio
@@ -23,12 +27,13 @@ def cli(wio):
     '''
     mserver = wio.config.get("mserver", None)
     if mserver:
-        click.echo(click.style('> ', fg='green') + "Current main server is: " +
+        click.echo(click.style('> ', fg='green') + "Current server is: " +
             click.style(mserver, fg='green'))
-        if click.confirm(click.style('Would you like log in with a different main server?', bold=True), default=False):
-            login_server(wio)
+        if click.confirm(click.style('Would you like login with a different server?', bold=True), default=False):
+            choise_server(wio)
     else:
-        login_server(wio)
+        choise_server(wio)
+        
     email = click.prompt(click.style('? ', fg='green') +
         click.style('Please enter your email address', bold=True), type=str)
     password = click.prompt(click.style('? ', fg='green') +
@@ -37,28 +42,21 @@ def cli(wio):
     thread = termui.waiting_echo("Sending login details...")
     thread.daemon = True
     thread.start()
-    params = {"email":email, "password":password}
-    api_prefix = wio.config.get("mserver", None)
+            
     try:
-        r = requests.post("%s%s" %(api_prefix, login_endpoint), params=params, timeout=10, verify=verify)
-        r.raise_for_status()
-        json_response = r.json()
-    except requests.exceptions.HTTPError as e:
-        thread.stop('')
-        thread.join()
-        if r.status_code == 400:
-            error = r.json().get("error", None)
-            click.secho(">> %s" %error, fg='red')
+        if wio.config.get("server") == 'Customize':
+            server_url = wio.config.get("mserver")
+            json_response = login_wio(server_url, email, password)
+            token = json_response['token']
         else:
-            click.secho(">> %s" %e, fg='red')
-        return
+            json_response = login_seeed(email, password)
+            token = json_response['data']['token']
     except Exception as e:
         thread.stop('')
         thread.join()
         click.secho(">> %s" %e, fg='red')
         return
 
-    token = json_response.get("token", None)
     wio.set_config('email', email)
     wio.set_config('token', token)
 
@@ -66,3 +64,40 @@ def cli(wio):
     thread.join()
     click.secho("\r> ", fg='green', nl=False)
     click.echo("Successfully completed login!")
+
+def get_sign(url, time):
+    APPID = config.SEEED_APPID
+    APPKEY = config.SEEED_APPKEY
+    COMMON = config.SEEED_COMMON
+    li = [APPID, APPKEY, COMMON, url, str(time)]
+    li.sort()
+    sign = ''
+    for l in li:
+        sign += l
+    h = hashlib.sha1(sign)
+    return h.hexdigest()
+    
+def login_seeed(email, passwd):
+    url = "http://bazaar.seeed.cc/api/index.php?r=common/user/login"
+    timestamp = int(time.time())
+    payload = dict(
+        email=email,
+        password=passwd,
+        source='4',
+        api_key='wiolink',
+        timestamp = timestamp,
+        sign = get_sign('r=common/user/login', timestamp))
+    r = requests.post(url, data=payload)
+    res = r.json()
+    if res.get('errorcode') != 0:
+        raise Exception('error', res.get('msgs'))
+    return res
+    
+def login_wio(server_url, email, passwd):
+    payload = dict(email=email, password=passwd)
+    r = requests.post("%s%s" %(server_url, login_endpoint), data=payload,
+        timeout=10, verify=verify)
+    res = r.json()
+    if r.status_code != 200:
+        raise Exception('error', res.get('error'))
+    return res
